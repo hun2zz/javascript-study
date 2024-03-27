e2a2ee746efcff0c2fafa2e8f7439bff4bd73600
hun2zz <tkdgnsdldkdlel@naver.com>
2024-03-27 13:53:57 +0900

    ✅ 콜백함수 까지 수강

---
+++ 1. core javascript/7. 스코프/3. const.js
@@ -0,0 +1,20 @@
+let x;
+console.log(x);
+
+const y = `메롱`;
+console.log(y);
+
+const PI = 3.14159265;
+const COLOR_GREEN = "#0f0";
+/*
+  $div.style.background = '#0f0';
+  */
+
+//상수는 객체의 불변성을 유지할 수 있다.
+let kim = {
+  name: `김철수`,
+  age: 30,
+};
+
+kim.age = 31;
+kimg.name = `마이클 킴`;
