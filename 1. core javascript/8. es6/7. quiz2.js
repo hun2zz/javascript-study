b44cf2b57ee1a32e77249830572851b99faaa74c
hun2zz <tkdgnsdldkdlel@naver.com>
2024-04-01 10:00:11 +0900

    ✅ dom조작 자식노드 탐색 수강

--- 1. core javascript/8. es6/7. quiz2.js
+++ 1. core javascript/8. es6/7. quiz2.js
@@ -1,5 +1,42 @@
-xdx
+const userList = [
+  {
+    account: "abc1234",
+    userName: "대길이",
+    job: "추노",
+    address: "서울",
+    hobbys: ["수영", "축구", "테니스"],
+    salary: 5400000,
+    age: 35,
+  },
+  {
+    account: "banana",
+    userName: "빠나나",
+    job: "과일",
+    address: "서울",
+    hobbys: ["푸드파이팅", "테니스"],
+    salary: 2700000,
+    age: 18,
+  },
+  {
+    account: "park1234",
+    userName: "주차왕",
+    job: "발렛파킹",
+    address: "경기",
+    hobbys: ["족구", "축구", "테니스", "영화감상"],
+    salary: 3900000,
+    age: 56,
+  },
+  {
+    account: "fire",
+    userName: "불꽃남자카리스마",
+    job: "게이머",
+    address: "서울",
+    hobbys: ["독서", "테니스"],
+    salary: 7900000,
+    age: 42,
+  },
 ];
+
 function find(condition) {
   for (const user of userList) {
     if (condition(user)) return user;
