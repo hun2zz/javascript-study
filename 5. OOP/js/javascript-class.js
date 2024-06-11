a29c312bf08896d33f98aecf2685598987b59904
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-11 10:34:36 +0900

    ✅ 자바스크립트 클래스

--- 5. OOP/js/javascript-class.js
+++ 5. OOP/js/javascript-class.js
@@ -1,2 +1,34 @@
+//객체의 설계도 class
 
-//
+class Pet {
+  //생성자를 통해 필드를 선언
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  introduce() {
+    console.log(`내 이름은 ${this.name}이구요. 나이는 ${this.age}살 입니다.`);
+  }
+}
+
+//상속
+class Dog extends Pet {
+  constructor(name, age, hobby) {
+    super(name, age);
+    this.hobby = hobby;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, sleep) {
+    super(name, age);
+    this.sleep = sleep;
+  }
+}
+const dog = new Dog("초코", 3, '산책가기');
+console.log(dog);
+const cat = new Cat("냐옹미", 2, '캣타워에서 낮잠');
+console.log(cat);
+
+console.log(`============================`);
+dog.introduce();
