bd03634ec22ed74cbdeb2007d170e70f1534be52
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-11 12:12:17 +0900

    ✅ component

--- 5. OOP/js/components/ShoppingCart.js
+++ 5. OOP/js/components/ShoppingCart.js
@@ -1,16 +1,20 @@
-class ShoppingCart {
-  constructor() {
-    //장바구니에 담은 producteㅡㄹ을 저장
-    this.cartItmes = [];
+import Component from "./Component.js";
+
+class ShoppingCart extends Component {
+
+  constructor(tagId) {
+    super(tagId);
+    // 장바구니에 담은 product들을 저장
+    this.cartItems = [];
   }
+
   render() {
-    const $section = document.createElement("section");
-    $section.classList.add("cart");
-    $section.innerHTML = `
-    <h2>총액 0원</h2>
-    <button>주문하기</button>
+    const childHtml = `
+      <h2>총액 0원</h2>
+      <button>주문하기</button>
     `;
-    return $section;
+
+    this.createElement('section', 'cart', childHtml);
   }
 }
 
